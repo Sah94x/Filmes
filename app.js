013437a107032932a1159e8c924c41d82091ccff
Sah94x <sabrina.fsayu@gmail.com>
2024-09-09 15:47:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+function pesquisar() {
+        // Seleciona a seção onde os resultados serão exibidos
+        let section = document.getElementById("resultados-pesquisa");
+
+        let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+        // se campoPesquisa for uma string sem nada
+        if(campoPesquisa == "") {
+                section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar no campo de pesquisa</p>"
+                return
+        }
+
+        campoPesquisa = campoPesquisa.toLowerCase()
+
+        console.log(campoPesquisa);
+      
+        // Inicializa uma string vazia para armazenar os resultados
+        let resultados = "";
+        let titulo = "";
+        let descricao = "";
+        let tags = "";
+      
+        // Itera sobre cada dado da pesquisa e constrói o HTML dos resultados
+        for (let dado of dados) {
+                titulo = dado.titulo.toLowerCase()
+                descricao = dado.titulo.toLowerCase()
+                tags =dado.tags.toLowerCase()
+                // se titulo includes campoPesquisa
+                if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+                        // cria um novo elemento
+                        resultados += `
+                        <div class="item-resultado">
+                          <h2>
+                            <a href="#" target="_blank">${dado.titulo}</a>
+                          </h2>
+                          <p class="descricao-meta">${dado.descricao}</p>
+                          <a href=${dado.link} target="_blank">Mais informações</a> 
+                        </div>
+                      `;
+                }
+                
+        }
+
+        if (!resultados) {
+                resultados = "<p>Nada foi encontrado</p>"
+        }
+      
+        // Atualiza o conteúdo da seção com os resultados construídos
+        section.innerHTML = resultados;
+      }
+
+
+
+
+
+    
+
---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo : "Alien: Romulus",
+        descricao : "Um grupo de jovens colonizadores, em busca de um novo lar, se depara com uma estação espacial abandonada e aterrorizante. Isolados e indefesos, eles rapidamente descobrem que não estão sozinhos. Uma criatura primordial, a personificação do terror cósmico, espreita nas sombras, pronta para caçar e devorar seus invasores. A bordo da estação, a claustrofobia e o desespero se intensificam a cada momento, enquanto a linha entre a vida e a morte se torna cada vez mais tênue.",
+        link : "https://www.imdb.com/title/tt18412256/?ref_=ext_shr_lnk",
+        tags: "alienigena monstro nave"
+},
+{
+    titulo : "Jurassic Park",
+    descricao : "Um bilionário visionário cria um parque temático onde dinossauros clonados vivem em seu habitat natural. No entanto, quando um sistema de segurança falha, os dinossauros escapam e os visitantes se encontram em uma luta desesperada pela sobrevivência. A ilha se transforma em um caótico parque de diversões pré-histórico, onde a natureza selvagem e a tecnologia colidem de forma brutal.",
+    link : "https://www.imdb.com/title/tt0107290/", 
+    tags: "t-rex dino ilha"},
+    {
+        titulo : "Maze Runner",
+        descricao : "Thomas acorda em uma clareira cercada por altos muros, sem nenhuma memória de seu passado. Junto com outros adolescentes, ele descobre que vive em um labirinto gigante e em constante mudança. A cada dia, eles enfrentam novos desafios e perigos, enquanto tentam desvendar os mistérios por trás de sua prisão e encontrar uma saída. Em meio à luta pela sobrevivência, Thomas começa a questionar sua própria identidade e o verdadeiro propósito de sua existência.",
+        link : "https://www.imdb.com/title/tt1790864/",
+        tags: "monstros zumbi cura"},
+];
+
+
+
+
+
